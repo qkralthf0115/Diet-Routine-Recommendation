79438fd4a94fb7d6d5b82e12607a20e844045432
dusndusn <168628191+dusndusn@users.noreply.github.com>
2024-05-30 16:03:57 +0900

    Delete MealCommand.cpp

--- MealCommand.cpp
+++
@@ -1,9 +0,0 @@
-#include"MealCommand.h"
-
-MealCommand::MealCommand(std::shared_ptr<DietPlan> dietPlan) : dietPlan(dietPlan) {}
-
-void MealCommand::execute()
-{
-	dietPlan->generatePlan();
-	dietPlan->printPlan();
-}
