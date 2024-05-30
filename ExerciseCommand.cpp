cb0283eef95bf4961f262af7e93a3fa32fdb5e44
qkralthf0115 <qkralthf0115@naver.com>
2024-05-30 16:09:48 +0900

    make shared_Ptr exercise plan

--- ExerciseCommand.cpp
+++ ExerciseCommand.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <memory>
+#include "ExercisePlan.h"
 
 class ExerciseCommand {
 private:
-
+  std::shared_ptr<ExercisePlan> exercisePlan;
 public:
   virtual void execute() = 0;
 };
