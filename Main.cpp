03b365a77a6397c8ce82ec05f383240005890871
qkralthf0115 <qkralthf0115@naver.com>
2024-06-01 18:53:44 +0900

    make choice

--- Main.cpp
+++ Main.cpp
@@ -2,5 +2,15 @@
 #include "Menu.h"
 
 int main() {
+  Menu menu;
+  menu.setMenu();
 
+  int choice;
+  do {
+    menu.displayMenu();
+    std::cin >> choice;
+    menu.selectCommand(choice);
+  } while (choice != 0);
+  std::cout << "Exit the program." << std::endl;
+  return 0;
 }
