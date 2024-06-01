f34997580bf4b8a4bea3eed11748d83fffe9a645
qkralthf0115 <qkralthf0115@naver.com>
2024-06-01 22:51:46 +0900

    edit totalCalories range

--- WorkoutPlan.cpp
+++ WorkoutPlan.cpp
@@ -7,7 +7,7 @@ WorkoutPlan::WorkoutPlan() {
 void WorkoutPlan::generateDailyWorkout(const std::vector<WorkoutItem>& workoutList, int dailyCalories) {
   dailyWorkouts.clear();
   int totalCalories = 0;
-  while (totalCalories < dailyCalories) {
+  while (dailyCalories - 500 <= totalCalories && totalCalories <= dailyCalories + 500) {
     int randomIndex = rand() % workoutList.size();
     dailyWorkouts.push_back(workoutList[randomIndex]);
     totalCalories += workoutList[randomIndex].getCaloriesBurned();
