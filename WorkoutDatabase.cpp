8de9cebc5f6d311cf68488252cab9f9d35c7756c
qkralthf0115 <qkralthf0115@naver.com>
2024-06-01 22:11:05 +0900

    split header file

--- WorkoutDatabase.cpp
+++ WorkoutDatabase.cpp
@@ -1,45 +1,24 @@
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include "WorkoutItem.cpp"
-#include "BodyArea.h"
+#include "WorkoutDatabase.h"
 
-class WorkoutDatabase {
-private:
-  std::vector<WorkoutItem> workoutList;
-
-  BodyArea stringToBodyArea(const std::string& areaStr) {
-    if (areaStr == "UpperBody") return BodyArea::UpperBody;
-    else if (areaStr == "LowerBody") return BodyArea::LowerBody;
-    else if (areaStr == "Core") return BodyArea::Core;
-    else if (areaStr == "Back") return BodyArea::Back;
-    else if (areaStr == "FullBody") return BodyArea::FullBody;
-    else throw std::invalid_argument("Unknown body area: " + areaStr);
+void WorkoutDatabase::loadWorkoutList(const std::string& filename) {
+  std::ifstream file(filename);
+  std::string line;
+  while (std::getline(file, line)) {
+    std::istringstream iss(line);
+    std::string name, focusAreaStr, videoLink;
+    int caloriesBurned;
+    if (!(iss >> name >> focusAreaStr >> caloriesBurned >> videoLink)) { break; }
+    BodyArea focusArea = stringToBodyArea(focusAreaStr);
+    workoutList.push_back(WorkoutItem(name, focusArea, caloriesBurned, videoLink));
   }
+}
 
-
-public:
-  void loadWorkoutList(const std::string& filename) {
-    std::ifstream file(filename);
-    std::string line;
-    while (std::getline(file, line)) {
-      std::istringstream iss(line);
-      std::string name, focusAreaStr, videoLink;
-      int caloriesBurned;
-      if (!(iss >> name >> focusAreaStr >> caloriesBurned >> videoLink)) { break; }
-      BodyArea focusArea = stringToBodyArea(focusAreaStr);
-      workoutList.push_back(WorkoutItem(name, focusArea, caloriesBurned, videoLink));
+std::vector<WorkoutItem> WorkoutDatabase::filterByArea(BodyArea targetArea) {
+  std::vector<WorkoutItem> filteredList;
+  for (const auto& workout : workoutList) {
+    if (workout.getFocusArea() == targetArea) {
+      filteredList.push_back(workout);
     }
   }
-
-  std::vector<WorkoutItem> filterByArea(BodyArea targetArea) {
-    std::vector<WorkoutItem> filteredList;
-    for (const auto& workout : workoutList) {
-      if (workout.getFocusArea() == targetArea) {
-        filteredList.push_back(workout);
-      }
-    }
-    return filteredList;
-  }
-};
+  return filteredList;
+}
