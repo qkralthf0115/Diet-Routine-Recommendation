02e40894add8f7d9a1ca273d88f5bf0932d17ecb
qkralthf0115 <qkralthf0115@naver.com>
2024-06-02 00:48:03 +0900

    add name

--- MealCommand.h
+++ MealCommand.h
@@ -9,8 +9,10 @@ class MealCommand : public Command
 private:
 	std::shared_ptr<DietPlan> dietPlan;
 	FoodDatabase& db;
+	std::string name = "Meal Recommendation";
 public:
 	MealCommand(std::shared_ptr<DietPlan> dietPlan, FoodDatabase& db);
 	void execute() override;
+	std::string getName() const;
 
 };
