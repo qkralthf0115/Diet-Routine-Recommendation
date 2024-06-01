11b6ecc16ca59002459f0ba40225b59fe513dfbe
qkralthf0115 <qkralthf0115@naver.com>
2024-06-01 22:17:54 +0900

    split header

--- ExercisePlan.cpp
+++ ExercisePlan.cpp
@@ -1,44 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib> // 랜덤
-#include <ctime> // 랜덤 숫자 생성을 위한 시간
-#include "WorkoutItem.cpp"
+#include "ExercisePlan.h"
 
-class ExercisePlan {
-private:
-  std::string name;
-  int caloriesBurned;
-  std::vector<std::vector<WorkoutItem>> weeklyPlan;
+void ExercisePlan::generateWeeklyPlan(const std::vector<WorkoutItem>& workoutList, int exerciseDays, int dailyCalories) {
+  weeklyPlan.clear();
+  for (int i = 0; i < exerciseDays; ++i) {
+    WorkoutPlan workoutPlan;
+    workoutPlan.generateDailyWorkout(workoutList, dailyCalories);
+    weeklyPlan.push_back(workoutPlan);
+  }
+}
 
-public:
-  void generatePlan(const std::vector<WorkoutItem>& workouts, int exerciseDays) {
-    srand(static_cast<unsigned int>(time(0))); // 랜덤 숫자 생성
-
-    int dailyCalories = calculateDailyCalories();
-    for (int i = 0; i < 7; ++i) {
-      std::vector<WorkoutItem> dailyWorkouts;
-      int totalCalories = 0;
-      while (totalCalories < dailyCalories) {
-        int randomIndex = rand() % workouts.size();
-        dailyWorkouts.push_back(workouts[randomIndex]);
-        totalCalories += workouts[randomIndex].getCaloriesBurned();
-      }
-      weeklyPlan.push_back(dailyWorkouts);
+void ExercisePlan::printWeeklyPlan() const {
+  for (int i = 0; i < weeklyPlan.size(); ++i) {
+    std::cout << "Day " << (i + 1) << " Plan:" << std::endl;
+    const auto& dailyWorkouts = weeklyPlan[i].getDailyWorkouts();
+    for (const auto& workout : dailyWorkouts) {
+      std::cout << "- " << workout.getName() << " (" << workout.getCaloriesBurned() << " calories)" << std::endl;
+      std::cout << "  Video Link: " << workout.getVideoLink() << std::endl;
     }
   }
-
-  void printPlan() {
-    for (int i = 0; i < weeklyPlan.size(); ++i) {
-      std::cout << "Day " << (i + 1) << " Plan:" << std::endl;
-      for (const auto& workout : weeklyPlan[i]) {
-        std::cout << "- " << workout.getName() << " (" << workout.getCaloriesBurned() << " calories)" << std::endl;
-        std::cout << "  Video Link: " << workout.getVideoLink() << std::endl;
-      }
-    }
-  }
-
-  int calculateDailyCalories() {
-
-    return 500;
-  }
-};
+}
---
+++ ExercisePlan.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "WorkoutPlan.h"
+
+class ExercisePlan {
+private:
+  std::vector<WorkoutPlan> weeklyPlan;
+
+public:
+  void generateWeeklyPlan(const std::vector<WorkoutItem>& workoutList, int exerciseDays, int dailyCalories);
+  void printWeeklyPlan() const;
+};
