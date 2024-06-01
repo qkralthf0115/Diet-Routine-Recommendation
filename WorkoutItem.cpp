42babb48c557e24290700de1748e038a9c3f719b
qkralthf0115 <qkralthf0115@naver.com>
2024-06-01 22:31:38 +0900

    split header

--- WorkoutItem.cpp
+++ WorkoutItem.cpp
@@ -1,7 +1,9 @@
 #include <string>
 #include "WorkoutItem.h"
 
-
+WorkoutItem::WorkoutItem(const std::string& name, BodyArea focusArea, int caloriesBurned, const std::string& videoLink)
+  : name(name), focusArea(focusArea), caloriesBurned(caloriesBurned), videoLink(videoLink) {
+}
 std::string WorkoutItem::getName() const { return name; }
 BodyArea WorkoutItem::getFocusArea() const { return focusArea; }
 int WorkoutItem::getCaloriesBurned() const { return caloriesBurned; }
--- WorkoutItem.h
+++ WorkoutItem.h
@@ -1,6 +1,6 @@
+#pragma once
 #include <string>
 #include "BodyArea.h"
-#pragma once
 
 class WorkoutItem {
 private:
@@ -10,10 +10,7 @@ private:
   std::string videoLink;
 
 public:
-  WorkoutItem(const std::string& name, BodyArea focusArea, int caloriesBurned, const std::string& videoLink)
-    : name(name), focusArea(focusArea), caloriesBurned(caloriesBurned), videoLink(videoLink) {
-  }
-
+  WorkoutItem(const std::string& name, BodyArea focusArea, int caloriesBurned, const std::string& videoLink);
   std::string getName() const;
   BodyArea getFocusArea() const;
   int getCaloriesBurned() const;
