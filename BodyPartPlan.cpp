5e2f791a72a9c9d4a3897bef4876653a6731b9f4
qkralthf0115 <qkralthf0115@naver.com>
2024-06-01 19:36:47 +0900

    make `generate plan`

--- BodyPartPlan.cpp
+++ BodyPartPlan.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "BodyPartPlan.h"
+#include "ExercisePlan.h"
 
-void BodyPartPlan::generatePlan() {
-  std::cout << "Body part exercise plan generated." << std::endl;
+class BodyPartPlan : public ExercisePlan {
+private:
+  std::string bodyPart;
+public:
+  BodyPartPlan(std::string bodyPart) : bodyPart(bodyPart) {}
+  void generatePlan() {}
+};
+
+void BodyPartPlan::generatePlan(const ExerciseData& db) {
+  exercises = db.getExercisesByFocusArea(bodyPart);
+  c
+    caloriesPerWeek = 500 * exercises.size();
 }
