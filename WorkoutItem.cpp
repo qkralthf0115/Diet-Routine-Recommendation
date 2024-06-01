6fe080ad673c9e7141a43a4a05a145d036d0e60b
qkralthf0115 <qkralthf0115@naver.com>
2024-06-01 22:13:34 +0900

    change into header

--- WorkoutItem.cpp
+++ WorkoutItem.cpp
@@ -1,20 +1,8 @@
 #include <string>
-#include "BodyArea.h"
+#include "WorkoutItem.h"
 
-class WorkoutItem {
-private:
-  std::string name;
-  BodyArea focusArea;
-  int caloriesBurned;
-  std::string videoLink;
 
-public:
-  WorkoutItem(const std::string& name, BodyArea focusArea, int caloriesBurned, const std::string& videoLink)
-    : name(name), focusArea(focusArea), caloriesBurned(caloriesBurned), videoLink(videoLink) {
-  }
-
-  std::string getName() const { return name; }
-  BodyArea getFocusArea() const { return focusArea; }
-  int getCaloriesBurned() const { return caloriesBurned; }
-  std::string getVideoLink() const { return videoLink; }
-};
+std::string WorkoutItem::getName() const { return name; }
+BodyArea WorkoutItem::getFocusArea() const { return focusArea; }
+int WorkoutItem::getCaloriesBurned() const { return caloriesBurned; }
+std::string WorkoutItem::getVideoLink() const { return videoLink; }
