f2440b530362bb68ea8498bec9ae2fe920cbbdb1
qkralthf0115 <qkralthf0115@naver.com>
2024-06-01 19:51:04 +0900

    add `gernerate plan` in full body plan

--- FullBodyPlan.cpp
+++ FullBodyPlan.cpp
@@ -3,5 +3,12 @@
 
 class FullBodyPlan : public ExercisePlan {
 public:
-  void generatePlan() {}
+  void generatePlan(const ExerciseData& db) {}
 };
+
+void FullBodyPlan::generatePlan() {
+  exercises = db.getExercises();
+  for (const auto& exercise : exercises) {
+    caloriesPerWeek += exercise.burnedCalories;
+  }
+}
