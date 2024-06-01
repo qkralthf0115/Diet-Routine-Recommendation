1370ef97e538c879087ccc1e1af3e041e024296f
qkralthf0115 <qkralthf0115@naver.com>
2024-06-02 00:47:52 +0900

    add `getName`

--- MealCommand.cpp
+++ MealCommand.cpp
@@ -3,8 +3,7 @@
 #include<sstream>
 MealCommand::MealCommand(std::shared_ptr<DietPlan> dietPlan, FoodDatabase& db) : dietPlan(dietPlan), db(db) {}
 
-void MealCommand::execute()
-{
+void MealCommand::execute() {
 	std::vector<std::string> exclude;
 	std::string input;
 	std::cout << "Foods you do not want to eat (separate by spaces): ";
@@ -12,12 +11,15 @@ void MealCommand::execute()
 
 	std::stringstream ss(input);
 	std::string item;
-	while (ss >> item)
-	{
+	while (ss >> item) {
 		exclude.push_back(item);
 	}
 
 	db.setExclusion(exclude);
 	dietPlan->generatePlan();
 	dietPlan->printPlan();
+}
+
+std::string MealCommand::getName() const {
+	return name;
 }
