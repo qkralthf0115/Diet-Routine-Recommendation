1363bbd0db51a4c731e868768ff908db5ac53e03
qkralthf0115 <qkralthf0115@naver.com>
2024-06-01 19:50:35 +0900

    add name, caloriesPerWeek

--- ExercisePlan.h
+++ ExercisePlan.h
@@ -5,7 +5,8 @@
 class ExercisePlan {
 private:
   std::vector<Exercise> exercises;
-
+  std::string name;
+  int caloriesPerWeek = 0;
 
 public:
   virtual void generatePlan(const ExerciseData& db) = 0;
