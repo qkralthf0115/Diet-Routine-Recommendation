e0874ae3b87a5b834c49a451a102440d48ee5d48
qkralthf0115 <qkralthf0115@naver.com>
2024-06-02 23:23:57 +0900

    make userinfo

--- ExerciseCommand.cpp
+++ ExerciseCommand.cpp
@@ -1,6 +1,8 @@
 #include "ExerciseCommand.h"
 
-
+ExerciseCommand::ExerciseCommand() {
+  userInfo = std::make_shared<UserInfo>();
+}
 void ExerciseCommand::execute() {
   std::string targetAreaStr;
   int exerciseDays;
@@ -32,6 +34,7 @@ BodyArea ExerciseCommand::stringToBodyArea(const std::string& areaStr) {
   else throw std::invalid_argument("Unknown body area: " + areaStr);
 }
 
-int ExerciseCommand::calculateDailyCalories() {
-  return 500;
+double ExerciseCommand::calculateDailyCalories() {
+  double dailyCalories = userInfo->calculateExerciseCalories();
+  return dailyCalories;
 }
