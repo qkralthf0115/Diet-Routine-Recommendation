d7297fdded9aa295bc928edaaa6cd744c2c69a0c
qkralthf0115 <qkralthf0115@naver.com>
2024-06-02 23:20:16 +0900

    add userInfo

--- ExerciseCommand.h
+++ ExerciseCommand.h
@@ -1,17 +1,19 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <memory>
 #include "Command.h"
 #include "ExercisePlan.h"
 #include "WorkoutDatabase.h"
 #include "BodyArea.h"
+#include "UserInfo.h"
 
 class ExerciseCommand : public Command {
 private:
   BodyArea stringToBodyArea(const std::string& areaStr);
   int calculateDailyCalories();
   std::string name = "Exercise Recommendation";
-
+  std::shared_ptr<UserInfo> userInfo;
 public:
   void execute() override;
 };
