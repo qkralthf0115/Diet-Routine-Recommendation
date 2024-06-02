80da43613ff639e02ed06e6d7879734039270079
qkralthf0115 <qkralthf0115@naver.com>
2024-06-02 23:05:39 +0900

    fix typo

--- InputInfoCommand.h
+++ InputInfoCommand.h
@@ -3,10 +3,10 @@
 #include"Command.h"
 #include"UserInfo.h"
 
-class InputInfoCommand: public Command
+class InputInfoCommand : public Command
 {
 private:
     std::shared_ptr<UserInfo> userInfo;
 public:
     void execute() override;
-}
+};
