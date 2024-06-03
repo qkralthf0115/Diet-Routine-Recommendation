4196d29dba2f6dac00860540cd7c653c678525fb
qkralthf0115 <qkralthf0115@naver.com>
2024-06-03 18:07:57 +0900

    add colored statement

--- Main.cpp
+++ Main.cpp
@@ -24,11 +24,12 @@ int main() {
   int temp;
   int choice;
   do {
+    std::cout << "\033[1;32m" << "Choose a command (1-3) or -1 to exit:" << "\033[1;32m" << std::endl;
     menu.displayMenu();
     std::cin >> temp;
     choice = temp - 1;
     menu.selectCommand(choice);
-  } while (choice != 0);
+  } while (choice != -1);
   std::cout << "Exit the program." << std::endl;
   return 0;
 }
