1a3bac480c6fc49dd4544009d271d4593016ac49
연우 <yeomwoo020412@gmail.com>
2024-06-11 10:15:33 +0900

    feedbackComment

--- FeedbackComment.cpp
+++ FeedbackComment.cpp
@@ -2,10 +2,28 @@
 #include"FeedbackComment.h"
 #include<iostream>
 
-FeedbackComment::FeedbackComment(): exerciseRecord(exerciseRecord){}
+FeedbackComment::FeedbackComment(): exerciseRecord(exerciseRecord), mealRecord(mealRecord){}
 
 void FeedbackComment::generateComment()
 {
-    exerciseRecord.printRecord();
-    mealRecord.printRecord();
+    std::cout<<"Exercise Record: "<<std::endl;
+    if (exerciseRecord.isComplete())
+    {
+        exerciseRecord.printRecord();
+    }
+    else
+    {
+        std::cout<<"Please complete exercise record"<<std::endl;
+    }
+
+    std::cout<<"Meal Record: "<<std::endl;
+    if (mealRecord.isComplete())
+    {
+        mealRecord.printRecord();
+    }
+    else
+    {
+        std::cout<<"Please complete meal record"<<std::endl;
+    }
+
 }
