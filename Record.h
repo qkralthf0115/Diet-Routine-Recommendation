e5e9f549bafeec65ef0a04189445468f28591b92
qkralthf0115 <qkralthf0115@naver.com>
2024-06-14 09:47:21 +0900

    print

--- Record.h
+++ Record.h
@@ -3,6 +3,7 @@
 class Record
 {
 public:
-    virtual void inputRecord()=0;
-    virtual void updateWeeklyRecord()=0;
+    virtual void inputRecord() = 0;
+    virtual void updateWeeklyRecord() = 0;
+    virtual void printRecord() const = 0;
 };
